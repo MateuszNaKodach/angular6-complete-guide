db17c5f60fc9e2bfb5b6717d6847d3fdc2665e80
mnowak <mateusz.nowak@intenso.pl>
2018-06-26 14:30:25 +0200

    Add basic angular directives

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,15 @@ import {FormsModule} from '@angular/forms';
 import {ServerComponent} from './server/server.component';
 import { ServersComponent } from './servers/servers.component';
 import {HttpClientModule} from '@angular/common/http';
+import { UserListComponent } from './user-list/user-list.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     ServerComponent,
-    ServersComponent
+    ServersComponent,
+    UserListComponent
   ],
   imports: [
     BrowserModule,
